ba96b52ac1f8af1edc70355214a0f06eb7d5c3a8
Muhannad <w7sh.oman@gmail.com>
2023-10-26 17:38:39 +0400

    feat(telescope): add shortcut to lookup themes.

--- coc/lua/plugins/telescope.lua
+++ coc/lua/plugins/telescope.lua
@@ -3,9 +3,18 @@ if not status_ok then
   return
 end
 
+telescope.setup{
+  pickers = {
+    colorscheme = {
+      enable_preview = true,
+      theme = "dropdown"
+    }
+  }
+}
 local builtin = require('telescope.builtin')
 local opts = { noremap = true}
 vim.keymap.set('n', '<leader><leader>f', builtin.find_files, opts)
 vim.keymap.set('n', '<leader><leader>g', builtin.live_grep, opts)
 vim.keymap.set('n', '<leader><leader>b', builtin.buffers, opts)
 vim.keymap.set('n', '<leader><leader>h', builtin.help_tags, opts)
+vim.keymap.set('n', '<leader><leader>t', builtin.colorscheme, opts)
